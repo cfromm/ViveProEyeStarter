47e1855a8774d13fb7301b63c8f5abf7116cefcd
Gabriel Diaz <performlabrit@users.noreply.github.com>
2019-08-05 11:50:59 -0400

    Added VOR target and Groundplane

---
+++ Assets/ploaceRelToHead.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ploaceRelToHead : MonoBehaviour
+{
+    public float xOffset = -5;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // x = -5
+        // y = 1.1
+        
+        transform.position = new Vector3(Camera.main.transform.position.x + xOffset, Camera.main.transform.position.y, Camera.main.transform.position.z);
+
+
+    }
+
+   
+}
